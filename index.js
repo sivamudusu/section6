3d8f14dcba33cfddceda139c0c44f65f8bd60841
sivamudusu <108459073+sivamudusu@users.noreply.github.com>
2023-11-15 18:48:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const add = (a, b) => {
+export const add = (a, b) => {
   return a + b;
 };
 
-const multiply = (a, b) => a * b;
+export const multiply = (a, b) => a * b;
 
-const subtract = (a, b) => a - b;
+export const subtract = (a, b) => a - b;
 
-const divide = (a, b) => a / b;
+export const divide = (a, b) => a / b;
 
-module.exports = { add, multiply, subtract, divide };
+// module.exports = { add, multiply, subtract, divide };
